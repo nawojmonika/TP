dafaa26752111541137cd9bccd775614da9bfa55
nawojmonika <nawojmonika@gmail.com>
2018-10-12 12:52:12 +0200

    Base classes

--- zad1/ClassLibrary1/Class1.cs
+++ zad1/ClassLibrary1/Class1.cs
@@ -1,12 +1,48 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
-namespace ClassLibrary1
+namespace Biblioteka
 {
-    public class Class1
+    public class Wykaz
     {
+        public string Imie { get; set; }
+        public string Nazwisko { get; set; }
+        public string IdKarty { get; set; }
+        public ICollection<Katalog> WypozyczoneKsiazki { get; set; }
+        public DateTime DataZalozenia { get; set; }
+        public Boolean CzyKontoAktywne { get; set; }
+    }
+
+    public class Katalog
+    {
+        public string IdKsiazki { get; set; }
+        public string Autor { get; set; }
+        public DateTime DataWydania { get; set; }
+        public string Wydawnictwo { get; set; }
+        public ICollection<Kategoria> Kategorie { get; set; }
+    }
+
+    public class OpisStanu
+    {
+        public string IdKsiazki { get; set; }
+        public Boolean CzyWypozyczona { get; set; }
+        public Wykaz KtoWypozyczyl { get; set; }
+        public DateTime dataWypozyczenia { get; set; }
+        public DateTime DataOddania { get; set; }
+    }
+
+    public class Zdarzenie
+    {
+    }
+
+
+    public enum Kategoria
+    {
+        Biografia = 0,
+        Fantastyka = 1,
+        Historyczna = 2,
+        Poezja = 3,
+        Sensacja = 4,
+        Popularnonaukowe = 5
     }
 }
