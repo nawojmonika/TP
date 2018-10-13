8d3ddb5e76ad27b8deb6d48ccef9fb45b4a08b9d
nawojmonika <nawojmonika@gmail.com>
2018-10-13 21:43:18 +0200

    Added datacontex

---
+++ zad1/DataContex.cs
@@ -0,0 +1,14 @@
+﻿using Biblioteka;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+
+namespace zad1
+{
+    class DataContex
+    {
+        public List<Wykaz> Czytelnicy { get; set; }
+        public Dictionary<string, Katalog> Ksiazki { get; set; } // need to read some more information about the dictonary and keys
+        public ObservableCollection<Zdarzenie> Zdarzenia { get; set; }
+        public ObservableCollection<OpisStanu> OpisyStanu { get; set; }
+    }
+}
