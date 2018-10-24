0ad7e6e0495d5fb294699ff613e1a00069bee177
nawojmonika <nawojmonika@gmail.com>
2018-10-24 20:15:40 +0200

    some classes from part e)

--- zad1/DataRepository.cs
+++ zad1/DataRepository.cs
@@ -1,8 +1,12 @@
-﻿namespace zad1
+﻿using System;
+
+namespace zad1
 {
     class DataRepository
     {
         public API Api { private get; set; }
         public DataContex Contex { get; set; }
+        //TO DO: 
+        //Add CRUD methods and tests for it
     }
 }
