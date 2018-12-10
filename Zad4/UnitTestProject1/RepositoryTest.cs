c2fc423b24c4225ce8d772a0bef78605bff90d2c
grayrattus <arturdziedziczak0@gmail.com>
2018-12-10 19:17:15 +0100

    Add basic test setup

---
+++ Zad4/UnitTestProject1/RepositoryTest.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Repository;
+
+namespace UnitTestProject1
+{
+    [TestClass]
+    public class RepositoryTest
+    {
+        [TestMethod]
+        public void RepositoryConstructor()
+        {
+            IDataRepository dataRepository = new DataRepository();
+        }
+
+        [TestMethod]
+        public void addUserTest()
+        {
+            IDataRepository dataRepository = new DataRepository();
+            int previousReposSize = dataRepository.getUsers().Count;
+            dataRepository.addUser(new User() { Id = Guid.NewGuid(), Age = 23, Name = "Mariuszk", Active = false });
+            Assert.IsTrue(dataRepository.getUsers().Count == ++previousReposSize);
+        }
+    }
+}
