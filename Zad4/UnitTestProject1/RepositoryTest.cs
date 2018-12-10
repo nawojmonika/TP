398ae66c46e99da951367410ee1c42e91c579e15
grayrattus <arturdziedziczak0@gmail.com>
2018-12-10 19:22:23 +0100

    Add repository tests

--- Zad4/UnitTestProject1/RepositoryTest.cs
+++ Zad4/UnitTestProject1/RepositoryTest.cs
@@ -21,5 +21,28 @@ namespace UnitTestProject1
             dataRepository.addUser(new User() { Id = Guid.NewGuid(), Age = 23, Name = "Mariuszk", Active = false });
             Assert.IsTrue(dataRepository.getUsers().Count == ++previousReposSize);
         }
+
+        [TestMethod]
+        public void removeUserTest()
+        {
+            IDataRepository dataRepository = new DataRepository();
+            var users = dataRepository.getUsers();
+            int previousReposSize = dataRepository.getUsers().Count;
+            dataRepository.removeUser(users[0]);
+            Assert.IsTrue(dataRepository.getUsers().Count == --previousReposSize);
+        }
+
+        [TestMethod]
+        public void updateUserTest()
+        {
+            IDataRepository dataRepository = new DataRepository();
+            var newUser = new User() { Id = Guid.NewGuid(), Age = 66, Name = "Karol M", Active = false };
+            dataRepository.addUser(newUser);
+            const string newUserName = "NewName";
+            newUser.Name = newUserName;
+            dataRepository.updateUser(newUser);
+            var users = dataRepository.getUsers();
+            Assert.AreEqual(users[users.Count - 1].Name, newUserName);
+        }
     }
 }
